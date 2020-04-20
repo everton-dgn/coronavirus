946ad08d32ee7e4761310a6de7d2f48fed25d06a
Everton Toffanetto <contato@querocriarsite.com>
2020-04-20 11:40:59 -0300

    Ajuste simples

--- js/modules/spinner-load.js
+++ js/modules/spinner-load.js
@@ -1,2 +1,8 @@
+// import { $, $$ } from './help.js';
 
-
+// // esconde spinners do load após 2s // 
+// export const spinnerLoad = setInterval(function () {
+//     clearInterval(spinnerLoad);
+//     $('[data-fundo-spinner]').style.display = "none";
+//     teste;
+// }, 1900);
